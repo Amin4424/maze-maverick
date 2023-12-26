5363706aa3c832797ab8d74ddc5305cd6840c395
Amin <ma.armaghan83@gmail.com>
2023-12-26 11:24:23 +0330

    make easy map

--- main.cpp
+++ main.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 using namespace std;
 
+void map(int, int);
 int main()
 {
-
+    int row, col;
+    cin >> row >> col;
+    map(row, col);
     return 0;
+}
+//Map Section
+void map(int row, int col)
+{
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < col; j++)
+    {
+      cout << "+----";
+    }
+    cout << "+" << endl;
+    for (int j = 0; j < col; j++)
+    {
+      cout << "| 01 ";
+    }
+    cout << "|"<<endl;
+  }
+  for (int j = 0; j < col; j++)
+  {
+    cout << "+----";
+  }
+  cout << "+" << endl;
 }
