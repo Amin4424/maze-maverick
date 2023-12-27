a4f099a1316db2bc3fc9f7d3738b63f30034b7a0
Amirrez4 <haveshkiamir@gmail.com>
2023-12-27 21:13:14 +0330

    Added menu section

--- main.cpp
+++ main.cpp
@@ -1,13 +1,54 @@
 #include <iostream>
+#include <stdlib.h>
+#include <conio.h>
 using namespace std;
 
 void map(int, int);
 int main()
 {
-    int row, col;
-    cin >> row >> col;
-    map(row, col);
-    return 0;
+    while (1)
+    {
+        cout<<"1. Create a New Map"<<endl;
+        cout<<"2. Playground"<<endl;
+        cout<<"3. Solve a Maze"<<endl;
+        cout<<"4. History"<<endl;
+        cout<<"5. Leaderboard"<<endl;
+        cout<<"6. Exit"<<endl;
+
+        char input = _getch();
+        switch (input)
+        {
+        case '1':
+        system("cls");
+            break;
+        
+        case '2':
+        system("cls");
+            break;
+        
+        case '3':
+        system("cls");    
+            break;
+        
+        case '4':
+        system("cls");    
+            break;
+        
+        case '5':
+        system("cls");    
+            break;
+        
+        case '6':
+        system("cls");    
+            break;
+        
+        default:
+        system("cls");
+        cout << "Please press a valid key" << endl;
+            break;
+        }
+    }
+    
 }
 //Map Section
 void map(int row, int col)
